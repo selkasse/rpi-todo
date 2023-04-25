154dffc47ad9fc13e1abd94dbb183d13e7a8ec1b
Sharif Elkassed <selkasse@gmu.edu>
2023-04-25 08:17:09 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+// import express from "express";
+const express = require("express");
+const { Gpio } = require("onoff");
+const app = express();
+const port = 3000;
+
+const redLedOut = new Gpio(18, "out");
+const greenLedOut = new Gpio(25, "out");
+let isComplete = false;
+
+app.get("/check", (req, res) => {
+  res.send("you have checked the item");
+  console.log("item was checked");
+  isComplete = true;
+});
+
+app.get("/uncheck", (req, res) => {
+  res.send("you have unchecked the item");
+  console.log("item was unchecked");
+  isComplete = false;
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+if (isComplete) {
+  greenLedOut.writeSync(1);
+} else {
+  redLedOut.writeSync(1);
+}
